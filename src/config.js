ca3c1a3fe89235ac05a27a8bb261544c512538c1
asalia-1 <asalia@wesleyan.edu>
2020-01-02 13:16:24 -0500

    finished routing and AWS config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "devnotes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "http://pl4fy23bgj.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_CogT2aHLU",
+    APP_CLIENT_ID: "i9fcqe2h9vo42pvp7p3f7broc",
+    IDENTITY_POOL_ID: "us-east-2:ffe9335c-8ebe-40d8-9e01-6e83b3a5d462"
+  }
+};
