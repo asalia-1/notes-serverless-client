18e4d407460ec1b76df5c0dea66d56309b97911a
asalia-1 <asalia@wesleyan.edu>
2020-01-07 10:47:54 -0500

    file update

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
 import "./App.css";
 import Routes from "./Routes";
 
 function App(props) {
-  return (
-    <div className="App container">
-      <Navbar fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/"> Scratch</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight>
-            <LinkContainer to="/signup">
-              <NavItem href="/signup">Signup</NavItem>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <NavItem href="/login">Login</NavItem>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+  const [isAuthenticated, setAuthentication] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
 
-      <Routes />
-    </div>
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      setAuthentication(true);
+    } catch (e) {
+      if (e !== "No current user") {
+        alert(e);
+      }
+    }
+    setIsAuthenticating(false);
+  }
+
+  async function handleLogout() {
+    await Auth.signOut();
+
+    setAuthentication(false);
+    props.history.push("/login");
+  }
+
+  return (
+    !isAuthenticating && (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/"> Scratch !</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              {isAuthenticated ? (
+                <NavItem onClick={handleLogout}>Logout</NavItem>
+              ) : (
+                <>
+                  <LinkContainer to="/signup">
+                    <NavItem href="/signup">Signup</NavItem>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <NavItem href="/login">Login</NavItem>
+                  </LinkContainer>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Routes appProps={{ isAuthenticated, setAuthentication }} />
+      </div>
+    )
   );
 }
 
-export default App;
+export default withRouter(App);
